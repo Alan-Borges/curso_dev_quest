22351e698a31d0d8c60b1226b0a9295370d08125
Alan Borges <158064983+Alan-Borges@users.noreply.github.com>
2024-09-18 19:07:22 -0300

    Add files via upload

---
+++ 11-modulo-javascript-basico/11-funcoes/index.js
@@ -0,0 +1,33 @@
+/* 
+    function nomeDaFuncao(){
+        <bloco da execução>
+    }
+
+    nomeDaFuncao()
+*/
+
+/* 
+    function nomeDaFuncao(parametro 1, parametro 2){
+        <bloco da execução>
+    }
+
+    nomeDaFuncao(parametro 1, parametro 2)
+*/
+
+function incentivarQuester(nomeQuester = 'Quester'){
+    alert('Muito bem ' + nomeQuester + ', você chegou ao JS, continue assim!')
+}
+
+incentivarQuester('Alan')
+
+// function soma(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = soma(1, 3)
+// console.log(resultadoDaSoma) //4
+
+// resultadoDaSoma = soma(resultadoDaSoma, 10)
+// console.log(resultadoDaSoma) //14
+
+
